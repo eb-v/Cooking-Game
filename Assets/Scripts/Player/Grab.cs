3bee19987dfe4e8da037b0a7d0b87ff0c1efcd7a
Klawedd <joshmiguel143@hotmail.com>
2025-09-13 15:32:56 -0500

    Added JointManager script and physics material for feet

--- Assets/Scripts/Player/Grab.cs
+++ Assets/Scripts/Player/Grab.cs
@@ -2,47 +2,33 @@ using UnityEngine;
 
 public class Grab : MonoBehaviour
 {
-    [SerializeField] private GameObject playerRagdoll;
-    [SerializeField] private Transform leftArm;
-    [SerializeField] private Transform rightArm;
+    private ConfigurableJoint _armJoint;
 
     private void Awake()
     {
-        GenericEvent<OnLeftGrabInput>.GetEvent(playerRagdoll.GetInstanceID()).AddListener(OnLeftGrab);
-        GenericEvent<OnRightGrabInput>.GetEvent(playerRagdoll.GetInstanceID()).AddListener(OnRightGrab);
-        GenericEvent<OnLeftGrabReleased>.GetEvent(playerRagdoll.GetInstanceID()).AddListener(OnLeftGrabReleased);
-        GenericEvent<OnRightGrabReleased>.GetEvent(playerRagdoll.GetInstanceID()).AddListener(OnRightGrabReleased);
+        GenericEvent<OnLeftGrabInput>.GetEvent(gameObject.GetInstanceID()).AddListener(OnLeftGrab);
+        GenericEvent<OnRightGrabInput>.GetEvent(gameObject.GetInstanceID()).AddListener(OnRightGrab);
+        GenericEvent<OnLeftGrabReleased>.GetEvent(gameObject.GetInstanceID()).AddListener(OnLeftGrabReleased);
+        GenericEvent<OnRightGrabReleased>.GetEvent(gameObject.GetInstanceID()).AddListener(OnRightGrabReleased);
+        _armJoint = GetComponent<ConfigurableJoint>();
     }
 
     private void OnLeftGrab()
     {
-        Vector3 currentRotation = leftArm.localEulerAngles;
-        currentRotation.x = 270f;
-        leftArm.rotation = Quaternion.Euler(currentRotation);
-        Debug.Log("Left Grab");
     }
 
     private void OnRightGrab()
     {
-        Vector3 currentRotation = rightArm.localEulerAngles;
-        currentRotation.x = 270f;
-        rightArm.rotation = Quaternion.Euler(currentRotation);
-        Debug.Log("Right Grab");
+       
     }
 
     private void OnLeftGrabReleased()
     {
-        Vector3 currentRotation = leftArm.localEulerAngles;
-        currentRotation.x = 0f;
-        leftArm.rotation = Quaternion.Euler(currentRotation);
-        Debug.Log("Left Grab Released");
+        
     }
 
     private void OnRightGrabReleased()
     {
-        Vector3 currentRotation = rightArm.localEulerAngles;
-        currentRotation.x = 0f;
-        rightArm.rotation = Quaternion.Euler(currentRotation);
-        Debug.Log("Right Grab Released");
+        
     }
 }
