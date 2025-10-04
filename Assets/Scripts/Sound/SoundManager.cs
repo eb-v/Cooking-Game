c37df4f149a5f014f62b9cbb22aff537eddb4257
alan <alanitoyah96@gmail.com>
2025-10-04 13:51:11 -0500

    Error Fixing

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 
-public class SoundManager : Monobehavior
+public class SoundManager
 {
     private void PlaySound(AudioClip audioClip, Vector3 position, float volume = 1f)
     {
