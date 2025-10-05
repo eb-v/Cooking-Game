d22b168094a37d592b519a87dd2e5a173261bbbd
alan <alanitoyah96@gmail.com>
2025-10-04 19:22:20 -0500

    Started the Game Timer Feature

---
+++ Assets/Scripts/UI/GameStartCountdownUI.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using TMPro;
+
+public class GameStartCountdownUI : MonoBehaviour
+{
+    [SerializeField] private TMP_Text countdownText;
+
+    private void Start()
+    {
+        KitchenGameManager.Instance.OnStateChanged += KitchenGameManager_OnStateChanged;
+
+        Hide();
+    }
+
+    private void KitchenGameManager_OnStateChanged(object sender, System.EventArgs e)
+    {
+        if (KitchenGameManager.Instance.IsCountdownToStartActive())
+        {
+            Show();
+        }
+        else
+        {
+            Hide();
+        }
+    }
+
+    private void Update()
+    {
+        countdownText.text = KitchenGameManager.Instance.GetCountdownToStartTimer().ToString();
+    }
+    private void Show()
+    {
+        gameObject.SetActive(true);
+    }
+    private void Hide()
+    {
+        gameObject.SetActive(false);
+    }
+}
