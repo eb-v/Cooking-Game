0bd0c9ba152293cbd4aa8e30b7fb991b69ec7d67
Derek Herrera <derekherrerq@gmail.com>
2025-10-06 20:05:51 -0500

    updated countdown and timer
    made it so the countdown actually pauses the game until it starts.
    
    also updated the ui of the timer

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -5,6 +5,7 @@ public class PauseMenu : MonoBehaviour
 {
     public GameObject container;
     private PlayerInput playerInput;
+    private bool isPausedByMenu = false;
 
     void Awake()
     {
@@ -38,6 +39,18 @@ public class PauseMenu : MonoBehaviour
         {
             playerInput.actions["Pause"].performed -= OnPause;
         }
+        
+        // If disabled while paused by menu, unpause
+        if (isPausedByMenu)
+        {
+            // Only unpause if countdown isn't active
+            if (!GameStartCountdownUI.CountdownIsActive)
+            {
+                Time.timeScale = 1;
+            }
+            GameStartCountdownUI.CountdownIsPaused = false;
+            isPausedByMenu = false;
+        }
     }
 
     private void OnPause(InputAction.CallbackContext context)
@@ -68,17 +81,45 @@ public class PauseMenu : MonoBehaviour
     {
         container.SetActive(true);
         Time.timeScale = 0;
+        isPausedByMenu = true;
+        
+        // If countdown is active, pause it too
+        if (GameStartCountdownUI.CountdownIsActive)
+        {
+            GameStartCountdownUI.CountdownIsPaused = true;
+            Debug.Log("Countdown paused by pause menu");
+        }
+        
+        Debug.Log("Game paused by pause menu");
     }
 
     public void ResumeGame()
     {
         container.SetActive(false);
-        Time.timeScale = 1;
+        
+        // Resume countdown if it was paused
+        if (GameStartCountdownUI.CountdownIsActive)
+        {
+            GameStartCountdownUI.CountdownIsPaused = false;
+            Debug.Log("Countdown resumed from pause menu");
+        }
+        
+        // Only unpause if countdown isn't active
+        // (If countdown is active, it will handle unpausing when done)
+        if (!GameStartCountdownUI.CountdownIsActive)
+        {
+            Time.timeScale = 1;
+        }
+        
+        isPausedByMenu = false;
+        Debug.Log("Game resumed from pause menu");
     }
 
     public void MainMenuButton()
     {
         Time.timeScale = 1;
+        isPausedByMenu = false;
+        GameStartCountdownUI.CountdownIsPaused = false;
         UnityEngine.SceneManagement.SceneManager.LoadScene("Main Menu Scene");
     }
 }
