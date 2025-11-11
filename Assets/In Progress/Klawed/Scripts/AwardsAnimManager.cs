a034488f3bf72e141cc58e33e8a7f73b61cd75f1
Klawedd <joshmiguel143@hotmail.com>
2025-11-10 19:07:53 -0600

    Created FireSystem

--- Assets/In Progress/Klawed/Scripts/AwardsAnimManager.cs
+++ Assets/In Progress/Klawed/Scripts/AwardsAnimManager.cs
@@ -8,19 +8,12 @@ public class AwardsAnimManager : MonoBehaviour
     [SerializeField] private List<GameObject> playersInScene;
     private void Start()
     {
-        //foreach (var player in playersInScene)
-        //{
-        //    int index = playersInScene.IndexOf(player);
-        //    //player.GetComponent<RagdollController>().enabled = false;
-        //    //foreach (Collider col in player.GetComponentsInChildren<Collider>())
-        //    //{
-        //    //    col.enabled = false;
-        //    //}
-        //    player.SetActive(false);
-        //    player.GetComponent<RagdollController>().GetPelvis().GetComponent<Rigidbody>().isKinematic = true;
-        //    player.SetActive(true);
+        GameObject playerContainer = GameObject.Find("Players");
 
-        //}
+        foreach (Transform player in playerContainer.transform)
+        {
+            playersInScene.Add(player.gameObject);
+        }
 
     }
 
