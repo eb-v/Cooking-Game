27520792165e8c60084c7b31247a8befea0a2403
abeaduhh <adiba.labonno@gmail.com>
2025-11-19 23:19:03 -0600

    fully functional dispenser

--- Assets/In Progress/Abeaduhhh/Scripts/SodaDispenser/Soda_menu.cs
+++ Assets/In Progress/Abeaduhhh/Scripts/SodaDispenser/Soda_menu.cs
@@ -1,9 +1,11 @@
-using UnityEngine;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
 
 public class Soda_menu : MonoBehaviour {
-    [Header("Display Spring")]
-    [SerializeField] private NonNormalizedSpringAPI displaySpring;
+    [Header("UI Elements")]
+    [SerializeField] private Image drinkImage;
 
     [Header("Available Drinks")]
     [SerializeField] private List<MenuItem> drinkCatalog;
@@ -11,8 +13,12 @@ public class Soda_menu : MonoBehaviour {
     public int currentDrinkIndex = 0;
 
     private void Start() {
-        GenericEvent<DPadInteractEvent>.GetEvent(gameObject.name).AddListener(OnDpadInteract);
-        GenericEvent<InteractEvent>.GetEvent(gameObject.name).AddListener(SelectDrink);
+        GenericEvent<DPadInteractEvent>.GetEvent(transform.parent.name).AddListener(OnDpadInteract);
+        GenericEvent<InteractEvent>.GetEvent(transform.parent.name).AddListener(SelectDrink);
+
+        Debug.Log(transform.parent.name);
+
+        UpdateDisplay(currentDrinkIndex);
     }
 
     private void OnDpadInteract(Vector2 input) {
@@ -24,34 +30,49 @@ public class Soda_menu : MonoBehaviour {
 
     private void NextDrink() {
         currentDrinkIndex++;
-
         if (currentDrinkIndex >= drinkCatalog.Count)
             currentDrinkIndex = 0;
 
         UpdateDisplay(currentDrinkIndex);
+        PrintCurrentDrink();
     }
 
     private void PreviousDrink() {
         currentDrinkIndex--;
-
         if (currentDrinkIndex < 0)
             currentDrinkIndex = drinkCatalog.Count - 1;
 
         UpdateDisplay(currentDrinkIndex);
+        PrintCurrentDrink();
     }
 
     private void UpdateDisplay(int drinkIndex) {
-        displaySpring.SetGoalValue(drinkIndex);
+        if (drinkCatalog == null || drinkCatalog.Count == 0) return;
+
+        MenuItem drink = drinkCatalog[drinkIndex];
+
+        if (drinkImage != null)
+            drinkImage.sprite = drink.GetFoodSprite();
+
     }
 
     public MenuItem GetCurrentDrink() => drinkCatalog[currentDrinkIndex];
 
     private void SelectDrink(GameObject player) {
+        Debug.Log("SelectDrink triggered! Player: " + player);
         MenuItem drink = GetCurrentDrink();
+        if (drink != null) {
 
-        GenericEvent<SodaSelectedEvent>.GetEvent("SodaDispenser")
-            .Invoke(drink);
+            GenericEvent<SodaSelectedEvent>.GetEvent("SodaDispenser").Invoke(drink);
+        }
+    }
 
-        Debug.Log("Selected drink: " + drink.name);
+    public void PrintCurrentDrink() {
+        MenuItem currentDrink = GetCurrentDrink();
+        if (currentDrink != null) {
+            Debug.Log("Current drink: " + currentDrink.name);
+        } else {
+            Debug.Log("No drink selected.");
+        }
     }
 }
