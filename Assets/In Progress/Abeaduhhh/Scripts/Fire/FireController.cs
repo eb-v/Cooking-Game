e06085593257d47186562958a6d9b2eeffa6b176
alan <alanitoyah96@gmail.com>
2025-11-25 20:29:33 -0600

    Updated Fire Audio

--- Assets/In Progress/Abeaduhhh/Scripts/Fire/FireController.cs
+++ Assets/In Progress/Abeaduhhh/Scripts/Fire/FireController.cs
@@ -1,8 +1,8 @@
 using UnityEngine;
-using UnityEngine.Events;
 using System.Collections;
 
-public class FireController : MonoBehaviour {
+public class FireController : MonoBehaviour
+{
     [Header("Fire Components")]
     [SerializeField] private ParticleSystem[] fireParticles;
 
@@ -11,7 +11,8 @@ public class FireController : MonoBehaviour {
 
     private bool burning = false;
 
-    private void Awake() {
+    private void Awake()
+    {
         if (fireParticles == null || fireParticles.Length == 0)
             fireParticles = GetComponentsInChildren<ParticleSystem>();
 
@@ -21,69 +22,82 @@ public class FireController : MonoBehaviour {
         GenericEvent<StopFireEvent>.GetEvent("FireController").AddListener(OnStopFireEvent);
     }
 
-    private void Start() {
-        //StartFire();
-    }
-
-    private void OnDestroy() {
+    private void OnDestroy()
+    {
         GenericEvent<StartFireEvent>.GetEvent("FireController").RemoveListener(OnStartFireEvent);
         GenericEvent<StopFireEvent>.GetEvent("FireController").RemoveListener(OnStopFireEvent);
-    }
 
-    private void OnStartFireEvent() {
-        StartFire();
-    }
-
-    private void OnStopFireEvent() {
-        StopFire();
-    }
-
-    public void StartFire() {
-    if (burning) return;
-    burning = true;
-
-    AudioManager.Instance?.PlaySFX("FireStart");
-
-    foreach (var ps in fireParticles) {
-        if (ps != null) {
-            ps.Clear();
-            ps.Play();
+        if (burning)
+        {
+            burning = false;
+            FireAudioManager.Instance?.UnregisterFire();
         }
     }
 
-    if (burnDuration > 0)
-        StartCoroutine(AutoStopFire());
-}
+    private void OnStartFireEvent() => StartFire();
+    private void OnStopFireEvent()  => StopFire();
 
+    public void StartFire()
+    {
+        if (burning) return;
+        burning = true;
 
-    private IEnumerator AutoStopFire() {
+        foreach (var ps in fireParticles)
+        {
+            if (ps != null)
+            {
+                ps.Clear();
+                ps.Play();
+            }
+        }
+
+        FireAudioManager.Instance?.RegisterFire();
+
+        if (burnDuration > 0)
+            StartCoroutine(AutoStopFire());
+    }
+
+    private IEnumerator AutoStopFire()
+    {
         yield return new WaitForSeconds(burnDuration);
         GenericEvent<StopFireEvent>.GetEvent("FireController").Invoke();
     }
 
-    public void StopFire() {
-    if (!burning) return;
-    burning = false;
-
-    AudioManager.Instance?.PlaySFX("FireOut");
-
-    foreach (var ps in fireParticles)
+    public void StopFire()
     {
-        if (ps != null)
-            ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
-    }
-}
-
-
-    public void StopFireImmediate() {
         if (!burning) return;
-        StopFire();
-    }
-
-    public void ResetFire() {
         burning = false;
 
-        foreach (var ps in fireParticles) {
+        foreach (var ps in fireParticles)
+        {
+            if (ps != null)
+                ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+
+        FireAudioManager.Instance?.UnregisterFire();
+    }
+
+    public void StopFireImmediate()
+    {
+        if (!burning) return;
+
+        burning = false;
+
+        foreach (var ps in fireParticles)
+        {
+            if (ps != null)
+                ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
+        }
+
+        FireAudioManager.Instance?.UnregisterFire();
+    }
+
+    public void ResetFire()
+    {
+        burning = false;
+
+        foreach (var ps in fireParticles)
+        {
             if (ps != null)
                 ps.Stop(true, ParticleSystemStopBehavior.StopEmittingAndClear);
         }
