7aa51b5af587fc9e6c05e64df9c0fdab29d91058
abeaduhh <adiba.labonno@gmail.com>
2025-11-28 02:15:25 -0600

    player cut anim, fixed earthquake, seperated npc

--- Assets/In Progress/Abeaduhhh/Scripts/Modifiers/Earthquake/EarthquakeCameraShake.cs
+++ Assets/In Progress/Abeaduhhh/Scripts/Modifiers/Earthquake/EarthquakeCameraShake.cs
@@ -1,35 +1,33 @@
 using UnityEngine;
+using System.Collections;
 
 public class EarthquakeCameraShake : MonoBehaviour {
-    private Vector3 originalPos;
     private bool shaking = false;
-
-    void Start() {
-        originalPos = transform.localPosition;
-    }
+    private Vector3 shakeOffset;
 
     public void Shake(float duration, float magnitude) {
         if (!shaking)
             StartCoroutine(ShakeRoutine(duration, magnitude));
     }
 
-    private System.Collections.IEnumerator ShakeRoutine(float duration, float magnitude) {
+    private IEnumerator ShakeRoutine(float duration, float magnitude) {
         shaking = true;
 
         float timer = 0f;
 
         while (timer < duration) {
-            float x = Random.Range(-1f, 1f) * magnitude;
-            float y = Random.Range(-1f, 1f) * magnitude;
-
-            transform.localPosition = originalPos + new Vector3(x, y, 0);
+            shakeOffset.x = Random.Range(-1f, 1f) * magnitude;
+            shakeOffset.y = Random.Range(-1f, 1f) * magnitude;
 
             timer += Time.deltaTime;
             yield return null;
         }
 
-        // reset camera
-        transform.localPosition = originalPos;
+        shakeOffset = Vector3.zero;
         shaking = false;
+    }
+
+    void LateUpdate() {
+        transform.localPosition += shakeOffset;
     }
 }
